48b0bc0a0f96b44755037a96c402d3d67049cfe7
takmatsumoto <motsidakaori@gmail.com>
2011-11-07 17:16:13 +0800

    新增兩個檔案，shader.cl 跟一個定義檔

---
+++ OpenCLTest2/HeadFileDef.h
@@ -0,0 +1,18 @@
+//
+//  HeadFileDef.h
+//  OpenCLTest2
+//
+//  Created by  on 2011/11/7.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef OpenCLTest2_HeadFileDef_h
+#define OpenCLTest2_HeadFileDef_h
+
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
+#include <CL/cl.h>
+#endif
+
+#endif
---
+++ OpenCLTest2/shader.cl
@@ -0,0 +1,12 @@
+//
+//  shader.c
+//  OpenCLTest2
+//
+//  Created by  on 2011/11/7.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+__kernel void addr (__global const float* a, __global const float* b, __global const float* result) 
+{
+    
+}
